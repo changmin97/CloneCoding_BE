fee147df513400958d56ffa0efc7152431dadaaa
cermen <yjs01016@naver.com>
2022-06-18 03:13:35 +0900

    회원가입 인증 코드 구현 (초안)

--- routes/users.js
+++ routes/users.js
@@ -3,27 +3,44 @@ const express = require("express");
 const User = require("../schemas/user");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
+const Joi = require("joi");
 //필요한 스키마 연결해주세요
+
+// 회원가입 조건
+const signUpSchema = Joi.object({
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] }}).required(),
+    nickname: Joi.string().pattern(new RegExp('^[0-9A-Za-z가-힣]{2,10}$')).required(),
+    password: Joi.string().pattern(new RegExp('^[0-9A-Za-z]{4,16}$')).required(),
+    passwordCheck: Joi.ref('password'),
+});
 
 // 회원가입
 router.post("/signup", async (req, res) => {
-    const { email, nickname, password, passwordCheck } = req.body;
+    try {
+        const { email, nickname, password, passwordCheck } = await signUpSchema.validateAsync(req.body);
 
-    // 비밀번호와 비밀번호 확인란이 일치하지 않을 경우
-    if (password !== passwordCheck) {
+        // 비밀번호와 비밀번호 확인란이 일치하지 않을 경우
+        if (password !== passwordCheck) {
+            res.status(400).send({
+                message: "비밀번호 확인란이 일치하지 않습니다.",
+                result: false,
+            });
+            return;
+        }
+
+        const user = new User({ email, nickname, password });
+        await user.save();
+
+        res.status(201).send({
+            message: "회원가입에 성공하였습니다.",
+            result: true,
+        });
+    } catch (err) {
         res.status(400).send({
-            errorMessage: "비밀번호 확인란이 일치하지 않습니다.",
+            message: err.details[0].message,
             result: false,
         });
-        return;
     }
-
-    const user = new User({ email, nickname, password });
-    await user.save();
-
-    res.status(201).send({
-        result: true,
-    });
 });
 
 // 로그인
