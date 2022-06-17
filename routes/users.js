7e880ac04cc1d7a9d09ea225b70af91ddf867b72
cermen <yjs01016@naver.com>
2022-06-17 23:56:15 +0900

    아이디/닉네임 중복체크 API 구현

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const User = require("../schemas/user");
-const Post = require("../schemas/post");
 const router = express.Router();
-const authMiddleware = require("../middlewares/authmiddleware.js");
+const jwt = require("jsonwebtoken");
 //필요한 스키마 연결해주세요
 
 // 회원가입
@@ -14,17 +13,6 @@ router.post("/signup", async (req, res) => {
     if (password !== passwordCheck) {
         res.status(400).send({
             errorMessage: "비밀번호 확인란이 일치하지 않습니다.",
-            result: false,
-        });
-        return;
-    }
-
-    const existUsers = await User.findOne({
-        $or: [{ email }, { nickname }],
-    });
-    if (existUsers) {
-        res.status(400).send({
-            errorMessage: "이메일 또는 닉네임이 이미 사용중입니다.",
             result: false,
         });
         return;
@@ -54,6 +42,48 @@ router.post("/login", async (req, res) => {
 
     res.send({
         token: jwt.sign({ userId: user.userId }, process.env.SECRET_KEY),
+        result: true,
+    });
+});
+
+/* 남은 일
+    아이디, 닉네임 중복체크 구현
+    회원가입 시 비밀번호 암호화 */
+
+// 아이디(이메일) 중복체크
+router.get("/idCheck/:username", async (req, res) => {
+    const email = req.params.username;
+    const existUser = await User.findOne({ email });
+    
+    if (existUser) {
+        res.status(400).send({
+            errorMessage: "사용중인 이메일입니다.",
+            result: false,
+        });
+        return;
+    }
+
+    res.send({
+        errorMessage: "사용 가능한 이메일입니다.",
+        result: true,
+    });
+});
+
+// 닉네임 중복체크
+router.get("/nicknameCheck/:nickname", async (req, res) => {
+    const nickname = req.params.nickname;
+    const existnickname = await User.findOne({ nickname });
+    
+    if (existnickname) {
+        res.status(400).send({
+            errorMessage: "사용중인 닉네임입니다.",
+            result: false,
+        });
+        return;
+    }
+
+    res.send({
+        errorMessage: "사용 가능한 닉네임입니다.",
         result: true,
     });
 });
