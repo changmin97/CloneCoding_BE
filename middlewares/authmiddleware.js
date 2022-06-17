5fcf5e644fc1df429b6e73f66d45ff1191f6a15f
cermen <yjs01016@naver.com>
2022-06-17 20:00:53 +0900

    미들웨어 구현

--- middlewares/authmiddleware.js
+++ middlewares/authmiddleware.js
@@ -1,28 +1,27 @@
-require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const User = require("../schemas/user");
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  const [authType, authToken] = authorization.split(" ");
+    try {
+        const { authorization } = req.headers;
+        const [authType, authToken] = authorization.split(" ");
 
-  if (!authToken || authType !== "Bearer") {
-    return res.status(401).send({errorMessage: "로그인 후 이용 가능한 기능입니다.",});
-  }
+        if (!authToken || authType !== "Bearer") {
+            res.status(401).send({
+                errorMessage: "로그인이 필요한 기능입니다.",
+            });
+            return;
+        }
 
-  try {
-    const nickname = jwt.verify(authToken, process.env.SECRET_KEY);
-    User.findOne({ nickname }).then((user) => {
-      res.locals.user = user;
-      next();
-    });
-  } catch (err) {
-    res.status(401).send({
-      errorMessage: "로그인 후 이용 가능한 기능입니다.",
-    });
-    return;
-  }
+        const { userId } = jwt.verify(authToken, process.env.SECRET_KEY);
+        User.findOne({ userId }).then((user) => {
+            res.locals.user = user;
+            next();
+        });
+    } catch (err) {
+        res.status(401).send({
+            errorMessage: "로그인이 필요합니다.",
+        });
+        return;
+    }
 };
-
-// const token = jwt.sign({ userId: Users.userId }, "my-secret-key");
-//     console.log(`${username}님이 로그인 하셨습니다.`)
