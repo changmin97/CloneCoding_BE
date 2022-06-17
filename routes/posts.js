5676d5d046cc86909df3fd8a5410b331184877d3
changmin97 <102282540+changmin97@users.noreply.github.com>
2022-06-18 00:50:36 +0900

    코드 수정

--- routes/posts.js
+++ routes/posts.js
@@ -3,7 +3,7 @@ const Comment = require("../schemas/comment");
 const Post = require("../schemas/post");
 const router = express.Router();
 const authMiddleware = require("../middlewares/authmiddleware.js");
-const user = require("../schemas/user");
+
 
 // 메인페이지
 router.get('/main',(req,res)=>{
@@ -11,12 +11,11 @@ router.get('/main',(req,res)=>{
 })
 // 게시물 조회 메인
 router.get("/post", authMiddleware, async (req, res, next) => {
-  const user = res.locals.user; //안쓰는데 왜 넣으셨어요 =ㅛ= 
   try {
     const posts = await Post.find({}).sort({ createAt: -1 });
     res.send(posts);
   } catch (error) {
-    res.status(500).json({ success: false, errorMessage: "실패했습니다." }); //message로 통일해주세용~ㅛ~
+    res.status(500).json({ success: false, message: "실패했습니다." }); //message로 통일해주세용~ㅛ~
   }
 });
 // 게시물 상세 조회 메인 상세
@@ -28,31 +27,37 @@ router.get("/post/postdetail/:postId", authMiddleware, async (req, res, next) =>
     return res.status(200).json({ postDetail, existcomments });
     
   } catch (error) {
-    res.status(400).json({ success: false, errorMessage: "실패했습니다." });
+    return res.status(400).json({ success: false, message: "실패했습니다." });
   }
 });
 
 // 게시물 작성
 router.post("/post/upload", authMiddleware, async (req, res) => {
-  const {nickname} = res.locals.user
-  const { title, content, imageUrl } = req.body
-  const maxPostId = await Post.findOne().sort("-postId");
-  let postId = 1;
-  if (maxPostId) {
-    postId = maxPostId.postId + 1;
+  try{
+    const {nickname} = res.locals.user
+      const { title, content, imageUrl } = req.body
+      const maxPostId = await Post.findOne().sort("-postId");
+      let postId = 1;
+      if (maxPostId) {
+        postId = maxPostId.postId + 1;
+      }
+      const createdPost = await Post.create({
+        postId,
+        title,
+        content,
+        imageUrl,
+        nickname,
+      });
+      return res.json({ post: createdPost });
+  }catch(err){
+    console.log(err)
+    return res.status(400).json({ success: false, message: "실패했습니다." });
   }
-  const createdPost = await Post.create({
-    postId,
-    title,
-    content,
-    imageUrl,
-    nickname,
-  });
-  res.json({ post: createdPost });
+  
 });
 //게시글 수정
-
 router.put('/post/postdetail/edit/:postId',authMiddleware, async(req,res)=>{
+  try{
     const postId = Number(req.params.postId)
     const [existPost] = await Post.find({ postId })
     const {user} = res.locals
@@ -66,14 +71,17 @@ router.put('/post/postdetail/edit/:postId',authMiddleware, async(req,res)=>{
     if(!content||!title||!imageUrl){
       return res.status(400).json({ result: false, message : "빈값을 채워주세요" })
     }
-    
     await Post.updateOne({postId}, {$set: {title, content, imageUrl} } )
     return res.status(200).json({ result: true })
+  }catch(err){
+    console.log(err)
+    return res.status(400).json({ success: false, message: "실패했습니다." });
+  }
+
 })
 
 // 게시물 삭제
 router.delete("/post/postdetail/delete/:postId", authMiddleware, async (req, res, next) => {
-
   try {
     const { postId } = req.params;
     const { user } = res.locals;
@@ -91,7 +99,7 @@ router.delete("/post/postdetail/delete/:postId", authMiddleware, async (req, res
   } catch (error) {
     return res
       .status(400)
-      .json({ success: false, errorMessage: "실패했습니다." });
+      .json({ success: false, message: "실패했습니다." });
   }
 });
 
